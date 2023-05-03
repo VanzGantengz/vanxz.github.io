0373e9f885ca79ed5432e3ce0bdc1909e8636e2d
Rio Agung Purnomo <rioagungpurnomo.ak@gmail.com>
2023-05-03 15:33:52 +0700

    upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rio Agung Purnomo
+Hi, introduce my name is Rio, I like to design websites, take natural scenery with my Canon DSLR camera to create beautiful memories that I get.
---
+++ assets/js/library.js
@@ -0,0 +1,51 @@
+var rap = {
+  github: function(){
+    const username = 'rioagungpurnomo';
+    const repoNames = [
+      'database',
+      'rioagungpurnomo.github.io'
+      ];
+    const apiUrl = `https://api.github.com/users/${username}/repos`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const filteredData = data.filter((repo) => !repoNames.includes(repo.name));
+
+      filteredData.forEach((repository) => {
+        const repositoryHtml = `
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box iconbox-blue">
+              <h4><a href="${repository.html_url}" target="_blank">${repository.name}</a></h4>
+              <p>${repository.description}</p>
+              <span><i class="bx bx-code"></i> ${repository.language}&nbsp;&nbsp;</span> <span><i class="bx bx-star"></i> ${repository.stargazers_count}</span>
+            </div>
+          </div>
+        `;
+        document.body.insertAdjacentHTML('beforeend', repositoryHtml);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  },
+  age: function(){
+  var now = new Date();
+  var yearNow = now.getFullYear();
+  var monthNow = now.getMonth() + 1;
+  var dateNow = now.getDate();
+  var differenceYear = yearNow - 2004;
+  
+  if (monthNow < 3) {
+    differenceYear--;
+  } else if (monthNow == 3) {
+    if (dateNow < 26) {
+      differenceYear--;
+    }
+  }
+  
+  var output = differenceYear + ' years old';
+
+  document.body.insertAdjacentHTML('beforeend', output);
+  }
+};
